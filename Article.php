289fcba07c57084ae1ba3aad5356d34c87e9d6ea
GiuseppeDalessio7 <g.dalessio03@gmail.com>
2023-10-20 19:00:04 +0200

    Refactoring Ex
    Finish commit

---
+++ Article.php
@@ -0,0 +1,18 @@
+<?php
+class article
+{
+    public $nameProduct;
+    public $price;
+    public $code;
+    public $format;
+    public $img;
+
+    public function __construct(string $nameProduct, int $price, int $code, $format, $img)
+    {
+        $this->nameProduct = $nameProduct;
+        $this->price = $price;
+        $this->code = $code;
+        $this->format = $format;
+        $this->img = $img;
+    }
+}
--- index.php
+++ index.php
@@ -6,27 +6,9 @@ if ($debug == true) {
 }
 
 require __DIR__ . "/layout.php";
+require __DIR__ . "/Article.php";
 require __DIR__ . "/Animals/Dog.php";
 require __DIR__ . "/Animals/Cat.php";
 require __DIR__ . "/Product/Food.php";
 require __DIR__ . "/Product/Toy.php";
 require __DIR__ . "/Product/Kennel.php";
-
-
-class article
-{
-    public $nameProduct;
-    public $price;
-    public $code;
-    public $format;
-    public $img;
-
-    public function __construct(string $nameProduct, int $price, int $code, $format, $img)
-    {
-        $this->nameProduct = $nameProduct;
-        $this->price = $price;
-        $this->code = $code;
-        $this->format = $format;
-        $this->img = $img;
-    }
-}
